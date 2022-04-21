d0c717460d51493b1ca4a34b91b3e33e61408d04
Mateusz Laska <mateo13311@gmail.com>
2022-04-21 19:26:54 +0200

    main page text update

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,10 +7,10 @@ function HeroSection() {
   return (
     <div className='hero-container'>
         <h1>O nas</h1>
-        <p>Jesteśmy serwisem hydraulicznym który upora się z powodzeniem z każdą hydrauliczną usterką. 
-          Świadczymy swoje usługi na terenie Rzeszowa i okolic. 
-          Nasza firma posiada wieloletnie doświadczenie dzięki czemu  sprawdzi się nie tylko w przypadku drobnych awarii ale również przy planowanych remontach czy modernizacji sieci gazowej, wodno- kanalizacyjnej oraz ogrzewania podłogowego. 
-          Zgłoś się po pomoc do nas  i zapomnij o kłopotliwych usterkach.</p>
+        <p>Jesteśmy firmą hydrauliczną która upora się z powodzeniem z każdą hydrauliczną usterką. 
+          Świadczymy swoje usługi na terenie Rzeszowa i okolic. <br/><br/>
+          Nasza firma posiada wieloletnie doświadczenie dzięki czemu  sprawdzi się nie tylko w przypadku drobnych awarii ale również przy planowanych remontach czy modernizacji sieci gazowej, wodno-kanalizacyjnej oraz centralnego ogrzewania. 
+          <br/><br/>Zgłoś się po pomoc do nas  i zapomnij o kłopotliwych usterkach.</p>
         <div className='hero-btns'>
         <Button
           className='btns'
