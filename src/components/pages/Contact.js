77c6aae515055c6eedae5228104f9d0bd439d551
Mateusz Laska <mateo13311@gmail.com>
2022-05-22 15:56:23 +0200

    changed text on main page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -8,8 +8,8 @@ export default function Contact() {
             <h1>Kontakt</h1>
             <p>NIP: 813-282-95-44</p>
             <p>email: SANIMAX2557@wp.pl</p>
-            <p>Zdzisław Pałka: +48 605 823 926</p>
-            <p>Krzysztof Pałka: +48 576 007 167</p>
+            <p>Telefon 1: +48 605 823 926</p>
+            <p>Telefon 2: +48 576 007 167</p>
         </div>
         </>
     ) 
--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -6,6 +6,7 @@ export default function Services() {
     return(
         <>
             <h1 className='services'>Zakres naszych prac</h1>
+            <p>bez wezla cieplnego i bez faktur, prysznice też wyjebać</p>
             <Footer />
         </>
     ) 
